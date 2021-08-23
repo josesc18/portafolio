b825f8aabe4a94e3a2f9613769de5a01dbe66b74
Jose <josedavid_san@hotmail.com>
2021-08-23 00:31:40 -0500

    routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutmeComponent } from './pages/aboutme/aboutme.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectComponent } from './pages/project/project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent, pathMatch: 'full' },
+  { path: "acerca-de-mi", component: AboutmeComponent },
+  { path: "proyectos", component: ProjectComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
